425f1ca73e4bbd61ed5a93a4fda731005f32fd41
Xuan Zhuo <xuanzhuo@linux.alibaba.com>
2020-11-11 17:16:36 +0800

    udp_send support gso test3 test4 test4 test10 test10 test100 test1000 test1000x test1000x test1000x test1000x test1000x test1000x test1000x test1000x test1000x test1000x test1000x test1000x test1000x test1000x test1000x test1000x test1000x test1000x

--- udp_send.c
+++ udp_send.c
@@ -1,81 +1,79 @@
 #include "uperf.h"
+#include "linux/udp.h"
+#define SOL_UDP 17
 
-static int udp_send_rate(int sockfd)
+
+static __thread int count;
+static __thread int total;
+static __thread struct timeval start;
+
+static int delay(struct timeval *s)
 {
-    	char buffer[22];
-
-    	struct sockaddr_in     servaddr;
-
-    	memset(&servaddr, 0, sizeof(servaddr));
-
-    	servaddr.sin_family = AF_INET;
-    	servaddr.sin_port = htons(config.port);
-    	servaddr.sin_addr.s_addr = inet_addr(config.server);
-
-    	int n, len;
-	struct timeval s, now;
-	int limit = 100, count = 0, total = 0, left;
+	int limit = 100, left;
 	int rate;
 	int64_t tm;
+    struct timeval now;
 	struct timespec sleep;
 
 	rate = config.rate / config.thread_n / 1000 / 100;// 10us
 
-	gettimeofday(&s, NULL);
+	++count;
 
-	while (1) {
-    		sendto(sockfd, buffer, sizeof(buffer), 0,
-		       (const struct sockaddr *) &servaddr,
-               	       sizeof(servaddr));
-		__sync_fetch_and_add(&config.reqs, 1);
-		++count;
+	if (count >= limit) {
+		total += count;
+		gettimeofday(&now, NULL);
+		tm = (now.tv_sec - s->tv_sec) * 1000 * 1000 + now.tv_usec - s->tv_usec;
 
-		if (count >= limit) {
-			total += count;
-			gettimeofday(&now, NULL);
-			tm = (now.tv_sec - s.tv_sec) * 1000 * 1000 + now.tv_usec - s.tv_usec;
+		left = total / rate * 10 - tm;
 
-			left = total / rate * 10 - tm;
+		sleep.tv_sec = 0;
+		sleep.tv_nsec = left * 1000;
 
-			sleep.tv_sec = 0;
-			sleep.tv_nsec = left * 1000;
+		if (left > 0)
+			nanosleep(&sleep, NULL);
 
-			if (left > 0)
-				nanosleep(&sleep, NULL);
-
-			count = 0;
-		}
+		count = 0;
 	}
 
-
-    	close(sockfd);
-
+    return 0;
 }
 
 static void udp_send()
 {
   	int sockfd, ret;
-    	char buffer[22];
+    char *buffer;
 	int v = 1;
+    struct sockaddr_in   caddr,  servaddr;
 
-    	struct sockaddr_in   caddr,  servaddr;
+    buffer = malloc(config.msglen);
 
-    	// Creating socket file descriptor
-    	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        	perror("socket creation failed");
-        	exit(EXIT_FAILURE);
-    	}
+    // Creating socket file descriptor
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
+    /* reuseport */
 	ret = setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &v, sizeof(v));
 	if (ret) {
 		printf("SO_REUSEPORT fail. %s\n", strerror(errno));
 		return;
 	}
 
+    /* gso */
+    if (config.gso) {
+        ret = setsockopt(sockfd, SOL_UDP, UDP_SEGMENT, &config.gso,
+                         sizeof(config.gso));
+	    if (ret) {
+		    printf("UDP_SEGMENT fail. %s\n", strerror(errno));
+		    return;
+	    }
+    }
 
-    	memset(&caddr, 0, sizeof(caddr));
+    /* bind */
+    memset(&caddr, 0, sizeof(caddr));
 	caddr.sin_family = AF_INET;
-	caddr.sin_port = config.sport;
+	caddr.sin_port = 0;
 	caddr.sin_addr.s_addr = inet_addr("0.0.0.0");
 
 	ret = bind(sockfd, (struct sockaddr *)&caddr, sizeof(caddr));
@@ -84,31 +82,30 @@ static void udp_send()
 		return;
 	}
 
+    /* the peer addr */
+    memset(&servaddr, 0, sizeof(servaddr));
 
-	if (config.rate) {
-		 udp_send_rate(sockfd);
-		 return;
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(config.port);
+    servaddr.sin_addr.s_addr = inet_addr(config.server);
 
-	}
 
-    	memset(&servaddr, 0, sizeof(servaddr));
+	gettimeofday(&start, NULL);
 
-    	// Filling server information
-    	servaddr.sin_family = AF_INET;
-    	servaddr.sin_port = htons(config.port);
-    	servaddr.sin_addr.s_addr = inet_addr(config.server);
-
-    	int n, len;
+    int n, len;
 
 	while (1) {
-    		sendto(sockfd, buffer, sizeof(buffer), 0,
-		       (const struct sockaddr *) &servaddr,
-               	       sizeof(servaddr));
+    	sendto(sockfd, buffer, config.msglen, 0,
+		       (const struct sockaddr *) &servaddr, sizeof(servaddr));
 		__sync_fetch_and_add(&config.reqs, 1);
+
+	    if (config.rate)
+            delay(&start);
 	}
 
 
-    	close(sockfd);
+    close(sockfd);
 }
 
 struct module mod_udp_send = {
